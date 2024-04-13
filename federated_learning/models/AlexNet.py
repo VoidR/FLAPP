30731a6bf629cba673b83b2b21e1d3772138fb35
pengdepan <1183359391@qq.com>
2024-04-13 19:24:23 +0800

    🐛(federated_learning/models/AlexNet.py)Fix input dimension and image size in AlexNet constructor

--- federated_learning/models/AlexNet.py
+++ federated_learning/models/AlexNet.py
@@ -1,27 +1,30 @@
+import torch
 import torch.nn as nn
 
 class AlexNet(nn.Module):
-
     def __init__(self, dim_in, num_classes, img_size):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(dim_in, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(dim_in, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 192, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
         )
+        # 计算经过特征提取层后的图像大小
+        size_after_features = ((img_size - 11) // 4 + 1) // 2 // 2 // 2
+        self.avgpool = nn.AdaptiveAvgPool2d((size_after_features, size_after_features))
         self.classifier = nn.Sequential(
             nn.Dropout(),
-            nn.Linear(256 * (img_size // 16) * (img_size // 16), 4096),
+            nn.Linear(256 * size_after_features * size_after_features, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(),
             nn.Linear(4096, 4096),
@@ -31,6 +34,7 @@ class AlexNet(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), -1)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
